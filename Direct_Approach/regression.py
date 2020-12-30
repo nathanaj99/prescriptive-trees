7bdbc9d5a1ddd09ad8b470af99601eed57d14af6
nathanaj99 <nathanaj@usc.edu>
2020-12-29 21:19:26 -0800

    update y

--- Direct_Approach/regression.py
+++ Direct_Approach/regression.py
@@ -4,38 +4,36 @@ from sklearn.tree import DecisionTreeRegressor
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-"""datasets = [1, 2, 3, 4, 5]
-probs = [0.1, 0.5, 0.9]
+datasets = [1, 2, 3, 4, 5]
 
 for dataset in datasets:
-    for prob in probs:
-        file_name = 'data_train_' + str(prob) + '_' + str(dataset) + '.csv'
-        file_name_enc = 'data_train_enc_' + str(prob) + '_' + str(dataset) + '.csv'
-        df = pd.read_csv('../data/Athey_v1_N_500/' + file_name)
-        df_enc = pd.read_csv('../data/Athey_v1_N_500/' + file_name_enc)
-        t_unique = df['t'].unique()
-        test = {}
+    file_name = 'data_train_' + str(dataset) + '.csv'
+    #file_name_enc = 'data_train_' + str(dataset) + '.csv'
+    df = pd.read_csv('../data/IST/' + file_name)
+    #df_enc = pd.read_csv('../data/IST/' + file_name_enc)
+    t_unique = df['t'].unique()
+    test = {}
 
-        for i in t_unique:
-            buffer = df[df['t'] == i]
-            X = buffer.iloc[:, :2]
-            y = buffer.iloc[:, -3]
-            #dr = DecisionTreeRegressor().fit(X, y)
-            lr = Lasso(alpha=0.1).fit(X, y)
-            #lr = LinearRegression().fit(X, y)
-            test[i] = lr
-
-
-        for i in range(2):
-            model = test[i]
-            X = df.iloc[:, :2]
-            prediction = model.predict(X)
-            df_enc['reg' + str(i)] = prediction
-
-        df_enc.to_csv('../data/direct_v1_500/' + file_name_enc, index=False)"""
+    for i in t_unique:
+        buffer = df[df['t'] == i]
+        X = buffer.iloc[:, :20]
+        y = buffer.iloc[:, 20]
+        lr = DecisionTreeRegressor().fit(X, y)
+        #lr = Lasso(alpha=0.1).fit(X, y)
+        #lr = LinearRegression().fit(X, y)
+        test[i] = lr
 
 
-df = pd.read_csv('cleaned_IST1.csv')
+    for i in range(1, 7):
+        model = test[i]
+        X = df.iloc[:, :20]
+        prediction = model.predict(X)
+        df['ml' + str(i)] = prediction
+
+    df.to_csv('../data/IST/' + file_name, index=False)
+
+
+"""df = pd.read_csv('cleaned_IST1.csv')
 
 df_train = df.iloc[:15200, :]
 df_test = df.iloc[15200:, :]
@@ -62,4 +60,4 @@ for i in range(1, 7):
     prediction = model.predict(X)
     df_train['reg' + str(i)] = prediction
 
-df_train.to_csv('IST_reg_train.csv', index=False)
+df_train.to_csv('IST_reg_train.csv', index=False)"""
