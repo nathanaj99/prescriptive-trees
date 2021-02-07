0c10e9a24a00c4f7a8810288a12730debdfc2821
nathanaj99 <nathanaj@usc.edu>
2021-02-07 13:40:43 -0500

    ml model added to data

--- Direct_Approach/regression.py
+++ Direct_Approach/regression.py
@@ -24,10 +24,10 @@ def warfarin():
     probs = [0.1, 0.33, 0.6, 0.85]
     for prob in probs:
         for dataset in datasets:
-            file_name = 'data_train_' + str(dataset) + '.csv'
-            file_name_enc = 'data_train_enc_' + str(dataset) + '.csv'
+            file_name = 'data_train_' + str(prob) + '_' + str(dataset) + '.csv'
+            file_name_enc = 'data_train_enc_' + str(prob) + '_' + str(dataset) + '.csv'
             # ----- CHANGE THE FILE PATH -----
-            file_path = '../data/Warfarin2/Warfarin_' + str(prob) + '_2000/'
+            file_path = '../data/Warfarin/3000/'
             df = pd.read_csv(file_path + file_name)
             df_enc = pd.read_csv(file_path + file_name_enc)
             t_unique = df['t'].unique()
@@ -43,12 +43,6 @@ def warfarin():
                 print(y.value_counts())
                 y_values = y.value_counts()
 
-                #lr = LogisticRegression().fit(X, y)
-
-                #lr = DecisionTreeRegressor().fit(X, y)
-                #lr = Lasso(alpha=0.1).fit(X, y)
-                #lr = LinearRegression().fit(X, y)
-                #lr = DecisionTreeClassifier().fit(X, y)
                 if y_values[1] > 5 and y_values[0] > 5:
                     smote = SMOTE(sampling_strategy=1.0, k_neighbors=5)
                 elif y_values[0] <= 5:
@@ -56,6 +50,8 @@ def warfarin():
                 elif y_values[1] <= 5:
                     smote = SMOTE(sampling_strategy=1.0, k_neighbors=y_values[1]-1)
                 X, y = smote.fit_resample(X, y)
+                if prob == 0.85:
+                    print(y.value_counts())
                 lr = RandomForestClassifier().fit(X, y)
                 test[i] = lr
 
@@ -65,7 +61,6 @@ def warfarin():
                 prediction = model.predict(X)
                 real = df['y' + str(i)]
                 tn, fp, fn, tp = skm.confusion_matrix(real, prediction).ravel()
-                #print(tn, fp, fn, tp)
                 tpr = tp / float(tp + fn)
                 tnr = tn / float(tn + fp)
                 print(tpr, tnr)
@@ -77,16 +72,12 @@ def warfarin():
                     tnr = tn / float(tn + fp)
                     print("REVISED:")
                     print(tpr, tnr)"""
-
-
                 print(skm.accuracy_score(real, prediction))
 
                 #handling the minority class
-
-
                 df_enc['ml' + str(i)] = prediction
 
-            #print(df_enc)
+            print(df_enc)
 
             df_enc.to_csv(file_path + file_name_enc, index=False)
 
@@ -98,7 +89,7 @@ def v1():
             file_name = 'data_train_' + str(prob) + '_' + str(dataset) + '.csv'
             file_name_enc = 'data_train_enc_' + str(prob) + '_' + str(dataset) + '.csv'
             # ----- CHANGE THE FILE PATH -----
-            file_path = '../data/Athey_v1_N_500/'
+            file_path = '../data/Athey_v1/500/'
             df = pd.read_csv(file_path + file_name)
             df_enc = pd.read_csv(file_path + file_name_enc)
             t_unique = df['t'].unique()
@@ -116,7 +107,7 @@ def v1():
                 # lr = LogisticRegression().fit(X, y)
 
                 # lr = DecisionTreeRegressor().fit(X, y)
-                lasso = Lasso(alpha=0.1).fit(X, y)
+                lasso = Lasso(alpha=0.08).fit(X, y)
                 linear = LinearRegression().fit(X, y)
                 # lr = DecisionTreeClassifier().fit(X, y)
                 test_linear[i] = linear
@@ -142,15 +133,15 @@ def v1():
 
             print(df_enc)
 
-            #df_enc.to_csv(file_path + file_name_enc, index=False)
+            df_enc.to_csv(file_path + file_name_enc, index=False)
 
 def v2():
-    probs = [0.1, 0.5, 0.75, 0.9]
+    probs = [0.1, 0.25, 0.5, 0.75, 0.9]
     for prob in probs:
         for dataset in datasets:
             file_name = 'data_train_' + str(prob) + '_' + str(dataset) + '.csv'
             # ----- CHANGE THE FILE PATH -----
-            file_path = '../data/Athey_v2_4000/'
+            file_path = '../data/Athey_v2/4000/'
             df = pd.read_csv(file_path + file_name)
             t_unique = df['t'].unique()
             test_linear = {}
@@ -164,7 +155,7 @@ def v2():
                 X = buffer.iloc[:, :3]
                 y = buffer['y']
 
-                lasso = Lasso(alpha=0.1).fit(X, y)
+                lasso = Lasso(alpha=0.08).fit(X, y)
                 linear = LinearRegression().fit(X, y)
                 # lr = DecisionTreeClassifier().fit(X, y)
                 test_linear[i] = linear
@@ -190,34 +181,6 @@ def v2():
 
             print(df)
 
-            #df.to_csv(file_path + file_name_enc, index=False)
+            df.to_csv(file_path + file_name, index=False)
 
 v2()
-"""df = pd.read_csv('cleaned_IST1.csv')
-
-df_train = df.iloc[:15200, :]
-df_test = df.iloc[15200:, :]
-print(df_train.shape)
-print(df_test.shape)
-
-df_test.to_csv('IST_reg_test.csv', index=False)
-
-t_unique = df_train['t'].unique()
-test = {}
-
-for i in t_unique:
-    buffer = df_train[df_train['t'] == i]
-    X = buffer.iloc[:, :20]
-    y = buffer.iloc[:, 20]
-    lr = DecisionTreeRegressor().fit(X, y)
-    #lr = Lasso(alpha=0.1).fit(X, y)
-    #lr = LinearRegression().fit(X, y)
-    test[i] = lr
-
-for i in range(1, 7):
-    model = test[i]
-    X = df_train.iloc[:, :20]
-    prediction = model.predict(X)
-    df_train['reg' + str(i)] = prediction
-
-df_train.to_csv('IST_reg_train.csv', index=False)"""
